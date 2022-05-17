9fe93e5e608e7ce3550faca41b90c5ad8963b19d
Ankitg996 <ankitg996@gmail.com>
2022-05-17 21:36:54 +0530

    update03

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -8,13 +8,13 @@ const generateShortUrl = async function (req, res) {
 
         if (!Object.keys(data).length) return res.status(400).send({ status: false, message: " You must provide data first " })
 
-        if (!(validUrl.isWebUri(data.longUrl.trim()))) return res.status(400).send({ status: false , message: "Please Provide a valid long Url"})
+        if (!(validUrl.isWebUri(data.longUrl.trim()))) return res.status(400).send({ status: false, message: "Please Provide a valid long Url" })
 
-        let checkUrl = await UrlModel.findOne({longUrl: data.longUrl})
+        let checkUrl = await UrlModel.findOne({ longUrl: data.longUrl })
 
-        if(checkUrl) return res.status(400).send({status: false, message: " Long url already Exists! and already shorted"})
+        if (checkUrl) return res.status(400).send({ status: false, message: " Long url already Exists! and already shorted" })
 
-        let shortUrlCode = RandomString.generate({length: 6, charset: "alphabetic"}).toLowerCase()
+        let shortUrlCode = RandomString.generate({ length: 6, charset: "alphabetic" }).toLowerCase()
 
         let shortUrl = `http://localhost:3000/${shortUrlCode}`
 
@@ -22,7 +22,7 @@ const generateShortUrl = async function (req, res) {
         data.shortUrl = shortUrl;
 
         let createUrl = await UrlModel.create(data)
-        return res.status(201).send({status: true, data: createUrl})     
+        return res.status(201).send({ status: true, data: createUrl })
 
     }
     catch (err) {
@@ -31,7 +31,19 @@ const generateShortUrl = async function (req, res) {
 }
 //---------------------Get Api------------------------------
 
+const getUrl = async function (req, res) {
+    try {
+        let data = req.params.urlCode
 
+        let urlData = await UrlModel.findOne({ urlCode: data })
+        if (!urlData) return res.status(400).send({ status: false, message: "Url code does not exists!" })
 
+        res.status(301).redirect(301, urlData.longUrl)
+    }
 
-module.exports = generateShortUrl
+    catch (err) {
+        res.status(500).send({ status: false, message: err.message })
+    }
+}
+
+module.exports = { generateShortUrl, getUrl }
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const urlController = require('../controllers/urlController')
 
-router.post('/url/shorten', urlController)
+router.post('/url/shorten', urlController.generateShortUrl)
+
+router.get('/:urlCode', urlController.getUrl)
 
 module.exports = router;
